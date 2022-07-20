0bd87deb5e0802d45dcca18a0c6199570ba9c69d
Samster91 <samster91>
2022-07-21 01:41:00 +0200

    NEW: fetch Idle earn opportunities

--- src/features/defi/contexts/DefiManagerProvider/DefiManagerProvider.tsx
+++ src/features/defi/contexts/DefiManagerProvider/DefiManagerProvider.tsx
@@ -1,17 +1,20 @@
 import { FoxyProvider } from 'features/defi/contexts/FoxyProvider/FoxyProvider'
 import { YearnProvider } from 'features/defi/contexts/YearnProvider/YearnProvider'
+import { IdleProvider } from 'features/defi/contexts/IdleProvider/IdleProvider'
 import React from 'react'
 import { Route, useLocation } from 'react-router-dom'
 import { NotFound } from 'pages/NotFound/NotFound'
 
 import { DefiModal } from '../../components/DefiModal/DefiModal'
 import { FoxyManager } from '../../providers/foxy/components/FoxyManager/FoxyManager'
+import { IdleManager } from '../../providers/idle/components/IdleManager/IdleManager'
 import { YearnManager } from '../../providers/yearn/components/YearnManager/YearnManager'
 import { DefiManagerContextProps, DefiManagerProviderProps, DefiProvider } from './DefiCommon'
 
 const DefiManagerContext = React.createContext<DefiManagerContextProps | null>(null)
 
 const DefiModules = {
+  [DefiProvider.Idle]: IdleManager,
   [DefiProvider.Yearn]: YearnManager,
   [DefiProvider.ShapeShift]: FoxyManager,
 }
@@ -27,19 +30,21 @@ export function DefiManagerProvider({ children }: DefiManagerProviderProps) {
   return (
     <DefiManagerContext.Provider value={null}>
       <YearnProvider>
-        <FoxyProvider>
-          {children}
-          {background && (
-            <Route
-              path='/defi/:earnType/:provider'
-              render={({ match: { params } }) => {
-                const { provider } = params
-                const Module = DefiModules[provider as DefiProvider]
-                return <DefiModal>{Module ? <Module /> : <NotFound />}</DefiModal>
-              }}
-            />
-          )}
-        </FoxyProvider>
+        <IdleProvider>
+          <FoxyProvider>
+            {children}
+            {background && (
+              <Route
+                path='/defi/:earnType/:provider'
+                render={({ match: { params } }) => {
+                  const { provider } = params
+                  const Module = DefiModules[provider as DefiProvider]
+                  return <DefiModal>{Module ? <Module /> : <NotFound />}</DefiModal>
+                }}
+              />
+            )}
+          </FoxyProvider>
+        </IdleProvider>
       </YearnProvider>
     </DefiManagerContext.Provider>
   )
